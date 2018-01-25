a15a83196dc3e246558c0534eee6d6d2ad3611ff
30-Forneus <rcapone@gmail.com>
2018-01-25 08:54:08 -0300

    Lemmatizer example

--- src/com/forneus/interview/resolvit/Result.java
+++ src/com/forneus/interview/resolvit/Result.java
@@ -3,12 +3,18 @@ package com.forneus.interview.resolvit;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 
+@JsonRootName("results")
 public class Result {
 	
 	@JsonProperty
 	private String word;
+	
+	@JsonIgnore
+	private String lemma;
 	
 	@JsonProperty("total-occurrences")
 	private Integer totalOcurrences;
@@ -16,15 +22,20 @@ public class Result {
 	@JsonProperty("sentence-indexes")
 	private List<Integer> sentenceIndexes;	
 	
-	public Result(String word) {
+	public Result(String word, String lemma) {
 		super();
 		this.word = word;
+		this.lemma = lemma;
 		this.totalOcurrences = 0;
 		this.sentenceIndexes = new ArrayList<Integer>();
 	}
 	
 	public String getWord() {
 		return word;
+	}
+
+	public String getLemma() {
+		return lemma;
 	}
 
 	public Integer getTotalOcurrences() {
